81cfeb70309e4dbc8ce012508ddaac92bac9a9fb
alejandro <alejandro@zeppelin.no>
2017-03-12 09:22:55 +0100

    Database set up

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Main starting point of the application
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser'); //use to parse incoming requests
 const morgan = require('morgan'); //logger for debug
 const router = require('./router');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Setup
+mongoose.connect('mongodb://localhost:/auth/auth'); //create database called auth
 
 //App Setup
 app.use(morgan('combined'));
